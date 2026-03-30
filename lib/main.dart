87dbe52ce8b5e3a1289a51e38e8badbd5974ca61
Master Daniel <engrdanywiss@gmail.com>
2026-03-30 12:07:24 +0100

    Initialize Flutter bindings and configure app themes for light and dark modes in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/services/ui/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
       title: "Application",
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
