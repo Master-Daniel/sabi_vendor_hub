5ee6dc33395eda5c7adb3a7c180a14bf71ec26ec
Master Daniel <engrdanywiss@gmail.com>
2026-03-30 12:06:07 +0100

    Add FileSelectorPlugin registration in GeneratedPluginRegistrant.swift for file selection support on macOS.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,7 +4,9 @@
 
 import FlutterMacOS
 import Foundation
+
 import connectivity_plus
+import file_selector_macos
 import firebase_analytics
 import firebase_core
 import firebase_messaging
@@ -15,6 +17,7 @@ import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
