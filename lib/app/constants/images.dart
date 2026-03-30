c5d3e8b42bb886e8c5eba0d2497469eea01b94a5
Master Daniel <engrdanywiss@gmail.com>
2026-03-30 12:07:53 +0100

    Add image constants for branding and shop setup in images.dart

--- lib/app/constants/images.dart
+++ lib/app/constants/images.dart
@@ -1,3 +1,14 @@
 class AppImages {
- 
+  AppImages._();
+  
+  static const String sabiLogoLight = 'assets/images/sabi-logo-light.png';
+  static const String sabiLogoDark = 'assets/images/sabi-logo-dark.png';
+  static const String sabiLogo = sabiLogoLight;
+
+  static const String googleLogo = 'assets/images/google_logo.png';
+  static const String appleLogo = 'assets/images/apple_logo.png';
+  static const String shopSetupCreateShop =
+      'assets/images/shop_setup_create_shop.png';
+  static const String shopSetupJoinShop =
+      'assets/images/shop_setup_join_shop.png';
 }
