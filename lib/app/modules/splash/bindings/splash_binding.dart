a68cdc53d98ddd77e5205171dd5cfa40bd8b0734
Master Daniel <engrdanywiss@gmail.com>
2026-03-30 12:12:26 +0100

    Add SplashBinding class to manage dependencies for SplashView, ensuring clean navigation without required GetX dependencies.

---
+++ lib/app/modules/splash/bindings/splash_binding.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+/// Splash route has no required GetX deps; navigation is driven from [SplashView].
+class SplashBinding extends Bindings {
+  @override
+  void dependencies() {}
+}
