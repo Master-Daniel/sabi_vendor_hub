8416bd7cfb5c45392749d04c561b969733ab44fc
Master Daniel <engrdanywiss@gmail.com>
2026-03-30 12:08:54 +0100

    Add route constants for Splash, Login, OTP, and Shop Setup in app_routes.dart

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,10 +3,20 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   Routes._();
+  static const SPLASH = _Paths.SPLASH;
+  static const LOGIN = _Paths.LOGIN;
+  static const OTP = _Paths.OTP;
+  static const SHOP_SETUP_BEGIN = _Paths.SHOP_SETUP_BEGIN;
+  static const SHOP_SETUP_PROFILE_DETAILS = _Paths.SHOP_SETUP_PROFILE_DETAILS;
   static const HOME = _Paths.HOME;
 }
 
 abstract class _Paths {
   _Paths._();
+  static const SPLASH = '/splash';
+  static const LOGIN = '/login';
+  static const OTP = '/otp';
+  static const SHOP_SETUP_BEGIN = '/shop-setup/begin';
+  static const SHOP_SETUP_PROFILE_DETAILS = '/shop-setup/profile-details';
   static const HOME = '/home';
 }
